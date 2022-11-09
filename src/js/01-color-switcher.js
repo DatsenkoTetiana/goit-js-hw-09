58755e2f54fb579b0aaaa0c36d8ffe46cb1e5dea
tetianadac <datsenkotetiana027@gmail.com>
2022-11-09 15:11:16 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+const dataStart = document.querySelector('[data-start]');
+const dataStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+dataStart.addEventListener('click', onStart);
+dataStop.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(getBg, 1000);
+
+  dataStart.toggleAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(timerId);
+
+  dataStart.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function getBg() {
+  body.style.backgroundColor = getRandomHexColor();
+}
