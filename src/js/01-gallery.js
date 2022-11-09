a0bc9dd4a4a1e5480d543bbbd1feac00ff2f1660
tetianadac <datsenkotetiana027@gmail.com>
2022-11-09 14:27:25 +0200

    add

---
+++ src/js/01-gallery.js
@@ -0,0 +1,6 @@
+// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
